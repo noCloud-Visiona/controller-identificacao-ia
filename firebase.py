85d584747df1a13ba7c7ab68abb5d496be749153
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2024-09-27 12:50:21 -0300

    Teste se firebase vai bloquear conexão

--- firebase.py
+++ firebase.py
@@ -1,9 +1,18 @@
+import json
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from dotenv import load_dotenv
 
-# Use uma conta de serviço.
-cred = credentials.Certificate('firebase/serviceAccount.json')
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obtém a chave do serviço do ambiente
+service_account_info = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+
+# Converte a string JSON para um dicionário
+cred = credentials.Certificate(json.loads(service_account_info))
 
 # Inicialize o app do Firebase Admin SDK.
 firebase_admin.initialize_app(cred)
