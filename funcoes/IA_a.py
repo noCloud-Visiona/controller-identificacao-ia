d729306b275dd113927c3932be98327067ab2cf3
Miguel <doxito007@gmail.com>
2024-11-11 14:07:22 -0300

    Refatorando Funções Para funcionar na nova lógica

--- funcoes/IA_a.py
+++ funcoes/IA_a.py
@@ -1,26 +1,29 @@
 from funcoes.funcoes_IA.segmentar_imagem import segmentar_imagens
 from funcoes.funcoes_IA.recortar_imagem import recortar_imagem_rgb
-from funcoes.funcoes_IA.remontagem_imagem import remontar, remontar_rgb, apply_mask
+from funcoes.funcoes_IA.remontagem_imagem import remontar, remontar_rgb, apply_mask, apply_inverse_mask
 from funcoes.funcoes_IA.porcentagem_nuvem import porcentagem_nuvem
 import funcoes.enums as Caminho
 import os
 
 def IA(image):
 
-    #recortar_imagem_rgb(tiff_path=image, image_output_dir=Caminho.Caminho.IMG_TILE.value, tile_size=640)
+    #tiles = recortar_imagem_rgb(tiff_path=image, image_output_dir=Caminho.Caminho.IMG_TILE.value, tile_size=1024)
 
     #segmentar_imagens()
 
-    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_TILE.value, tile_height=640, tile_width=640, tiles_per_col=90, tiles_per_row=88, filler_color=(0, 0, 0), tile_name="RGB", final_file_name="imagem_PNG_montada")
-    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_MARK.value, tile_height=640, tile_width=640, tiles_per_col=90, tiles_per_row=88, filler_color=(0, 0, 0), tile_name="RGB_masked_output_0", final_file_name="mask_image_PNG")
+    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_TILE.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB", final_file_name="imagem_PNG_montada")
+    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_MARK.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB_masked_output_0", final_file_name="mask_image_PNG")
 
-    apply_mask(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_nuvem_montada.png")
+    mask_np, image_np = apply_mask(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_nuvem_montada.png")
+    apply_inverse_mask(image_np=image_np, mask_np=mask_np, output_path="./imagem_final_cortada_sem_nuvens.png")
 
-    #remontar(tile_dir=Caminho.Caminho.IMG_MERGED.value, tile_height=640, tile_width=640, tiles_per_col=90, tiles_per_row=88, filler_color=(0, 0, 0), tiff_path=image)
-    #remontar(tile_dir=Caminho.Caminho.IMG_MARK.value, tile_height=640, tile_width=640, tiles_per_col=90, tiles_per_row=88, filler_color=(0, 0, 0), tile_name="RGB_masked_output_0", final_file_name="mask_image_final", tiff_path=image)
+
+
+    remontar(img_png="imagem_final_cortada_nuvem_montada", tiff_path=image, final_file_name="imagem_final_montada_nuvem")
+    remontar(img_png="imagem_final_cortada_sem_nuvens", tiff_path=image, final_file_name="imagem_final_cortada_sem_nuvens")
     
 
 
     #percent = porcentagem_nuvem(mask="./mask_image_final.png", img="./imagem_final_montada.png")
     print("Retornando Imagem")
-    return "./mask_image_final.tiff", "./imagem_final_montada.tiff" , 0
+    #return "./mask_image_final.tiff", "./imagem_final_montada.tiff" , 0
--- funcoes/funcoes_IA/remontagem_imagem.py
+++ funcoes/funcoes_IA/remontagem_imagem.py
@@ -21,12 +21,12 @@ def create_background_tile(tile_width, tile_height, filler_color):
 def process_tile(row_index, col_index, tile_width, tile_height, tile_name, tile_dir, filler_color):
     tile_filename = f"{row_index}_{col_index}_{tile_name}.png"
     tile_path = os.path.join(tile_dir, tile_filename)
-
+    
     if not os.path.exists(tile_path):
         placeholder_tile = Image.new('RGB', (tile_width, tile_height), filler_color)
         return placeholder_tile, (col_index * tile_width, row_index * tile_height)
 
-
+    print("Processando o tile:", tile_filename)
     tile = load_tile(tile_filename, tile_dir)
     current_tile_width, current_tile_height = get_tile_dimensions(tile)
 
@@ -68,6 +68,7 @@ def remontar_rgb(tile_dir, tile_width, tile_height, tiles_per_col, tiles_per_row
             executor.submit(process_tile, row, col, tile_width, tile_height, tile_name, tile_dir, filler_color): (row, col)
             for row in range(tiles_per_col)
             for col in range(tiles_per_row)
+
         }
 
         for future in concurrent.futures.as_completed(future_to_tile):
@@ -79,58 +80,31 @@ def remontar_rgb(tile_dir, tile_width, tile_height, tiles_per_col, tiles_per_row
     print("Imagem final ajustada montada com sucesso!")
 
 
-def remontar(tile_dir, tile_width, tile_height, tiles_per_col, tiles_per_row, filler_color, tiff_path, tile_name="RGB_merged_0", final_file_name="imagem_final_montada"):
-    block_size = 1024  # Tamanho do bloco de leitura
-    final_width = tile_width * tiles_per_row
-    final_height = tile_height * tiles_per_col
-    imagem_final = Image.new('RGB', (final_width, final_height))
-    
-    tiff_original = Image.open(tiff_path)  
-    metadados = tiff_original.tag_v2
-    print(f"Metadados originais: {metadados}")  # Imprime os metadados originais para depuração
-    metadados_filtrados = filtrar_metadados(metadados)  
-    print(f"Metadados filtrados: {metadados_filtrados}") 
+def remontar(img_png, tiff_path, final_file_name="imagem_final_montada"):
     tiff_original = gdal.Open(tiff_path)
     metadados = tiff_original.GetMetadata()
     geotransform = tiff_original.GetGeoTransform()
     projection = tiff_original.GetProjection()
-    for row_index in range(tiles_per_col):
-        for col_index in range(tiles_per_row):
-            tile_filename = f"{row_index}_{col_index}_{tile_name}.png"
-            print(tile_filename)
-            try:
-                tile = load_tile(tile_filename, tile_dir)
-            except FileNotFoundError:
-                print(f"Tile {tile_filename} não encontrado. Pulando.")
-                continue
-            # Verifica as dimensões do tile
-            current_tile_width, current_tile_height = get_tile_dimensions(tile)
-            x_position = col_index * tile_width
-            y_position = row_index * tile_height
-            # Criar um fundo do tamanho correto e colar o tile
-            background = Image.new('RGB', (tile_width, tile_height), filler_color)
-            background.paste(tile, (0, 0, current_tile_width, current_tile_height))
-            imagem_final.paste(background, (x_position, y_position))
-    imagem_array = np.array(imagem_final)
+
+    imagem_png = Image.open(img_png).convert("RGB")
+    final_width, final_height = imagem_png.size  
+    imagem_array = np.array(imagem_png)
 
     driver = gdal.GetDriverByName("GTiff")
-    output_tiff = driver.Create(final_file_name + ".tiff", final_width, final_height, 3, gdal.GDT_Byte, options=["COMPRESS=DEFLATE", "BIGTIFF=YES"])
+    output_tiff = driver.Create(
+        final_file_name + ".tiff", final_width, final_height, 3, gdal.GDT_Byte, options=["COMPRESS=DEFLATE", "BIGTIFF=YES"]
+    )
 
-    print("Output_tiff 0: ", output_tiff)
-
-    # Configurações de metadados e geoinformação
     output_tiff.SetGeoTransform(geotransform)
     output_tiff.SetProjection(projection)
     output_tiff.SetMetadata(metadados)
 
-    # Escrever cada banda na imagem
-    print("Escrever cada banda na imagem")
-    for i in range(3):  # Assumindo RGB, 3 bandas
+    for i in range(3):  
         output_tiff.GetRasterBand(i + 1).WriteArray(imagem_array[:, :, i])
 
-    print("Salvando Imagem:")
     output_tiff.FlushCache()
-    print("Imagem final montada com sucesso, com metadados preservados!")
+    print("Imagem PNG convertida para TIFF com sucesso, usando metadados do TIFF original!")
+
 
 def verify_image_with_pillow(image_path):
     try:
@@ -141,40 +115,21 @@ def verify_image_with_pillow(image_path):
         print("Erro ao verificar a imagem com Pillow.")
         raise ValueError(f"Erro ao verificar a imagem '{image_path}': {e}")
 
-def apply_inverse_mask(image_path, mask_path, output_path):
-    print("Procurando arquivos de imagem e máscara...")
-    if not os.path.exists(image_path):
-        raise FileNotFoundError(f"Arquivo de imagem '{image_path}' não encontrado.")
-    if not os.path.exists(mask_path):
-        raise FileNotFoundError(f"Arquivo de máscara '{mask_path}' não encontrado.")
-    
-    # Carrega a imagem com PIL e converte para o formato OpenCV
-    print("Carregando imagem com PIL e convertendo para OpenCV...")
-    try:
-        image = Image.open(image_path)
-        image = np.array(image)
-    except Exception as e:
-        raise ValueError(f"Erro ao carregar a imagem '{image_path}' com PIL: {e}")
-    
-    # Carrega a máscara com PIL, converte para escala de cinza e inverte a máscara
-    print("Carregando máscara com PIL, convertendo e invertendo...")
-    try:
-        mask = Image.open(mask_path).convert("L")  # "L" para escala de cinza
-        mask = np.array(mask)
-        mask = cv2.bitwise_not(mask) 
-    except Exception as e:
-        raise ValueError(f"Erro ao carregar a máscara '{mask_path}' com PIL: {e}")
-
-    # Verifica se a imagem e a máscara têm as mesmas dimensões
+def apply_inverse_mask(image_np, mask_np, output_path):
     print("Verificando o tamanho da máscara...")
-    if image.shape[:2] != mask.shape:
+    if image_np.shape[:2] != mask_np.shape:
         raise ValueError("A imagem e a máscara devem ter as mesmas dimensões.")
 
-    # Aplica a máscara inversa à imagem, removendo as áreas em branco
-    print("Aplicando máscara inversa à imagem...")
-    result = cv2.bitwise_and(image, image, mask=mask)
+    print("Invertendo a máscara...")
+    inverted_mask = cv2.bitwise_not(mask_np) 
 
-    # Salva o resultado
+    print("Aplicando máscara inversa à imagem...")
+    result = cv2.bitwise_and(image_np[:, :, :3], image_np[:, :, :3], mask=inverted_mask)  
+    result = cv2.cvtColor(result, cv2.COLOR_RGB2BGRA)  
+
+    transparent_pixels = (inverted_mask == 0)
+    result[transparent_pixels] = [0, 0, 0, 0]
+
     cv2.imwrite(output_path, result)
     print(f"Imagem recortada com máscara inversa salva em {output_path}")
 
@@ -188,24 +143,34 @@ def apply_mask(image_path, mask_path, output_path):
     
     print("Carregando imagem com PIL e convertendo para OpenCV...")
     try:
-        image = Image.open(image_path)
-        image = np.array(image)
+        image = Image.open(image_path).convert("RGBA")
+        image_np = np.array(image)
     except Exception as e:
         raise ValueError(f"Erro ao carregar a imagem '{image_path}' com PIL: {e}")
     
+    print("Removendo pixels completamente pretos...")
+    black_pixels = (image_np[:, :, :3] == [0, 0, 0]).all(axis=2)
+    image_np[black_pixels] = [0, 0, 0, 0] 
+
     print("Carregando máscara com PIL e convertendo para OpenCV...")
     try:
         mask = Image.open(mask_path).convert("L")  # "L" para escala de cinza
-        mask = np.array(mask)
+        mask_np = np.array(mask)
     except Exception as e:
         raise ValueError(f"Erro ao carregar a máscara '{mask_path}' com PIL: {e}")
 
     print("Verificando o tamanho da máscara...")
-    if image.shape[:2] != mask.shape:
+    if image_np.shape[:2] != mask_np.shape:
         raise ValueError("A imagem e a máscara devem ter as mesmas dimensões.")
 
     print("Aplicando máscara à imagem...")
-    result = cv2.bitwise_and(image, image, mask=mask)
+    result = cv2.bitwise_and(image_np[:, :, :3], image_np[:, :, :3], mask=mask_np) 
+    result = cv2.cvtColor(result, cv2.COLOR_RGB2BGRA)
+
+    result[black_pixels] = [0, 0, 0, 0]
 
     cv2.imwrite(output_path, result)
     print(f"Imagem recortada salva em {output_path}")
+    return mask_np, image_np
+
+   
