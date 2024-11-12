7cd83cc53c450f74bc54dee552261932b1b6e8d3
Miguel <doxito007@gmail.com>
2024-11-11 21:52:55 -0300

    Descomentando funcoes

--- funcoes/IA_a.py
+++ funcoes/IA_a.py
@@ -7,18 +7,18 @@ import os
 
 def IA(image):
 
-    #tiles = recortar_imagem_rgb(tiff_path=image, image_output_dir=Caminho.Caminho.IMG_TILE.value, tile_size=1024)
+    tiles = recortar_imagem_rgb(tiff_path=image, image_output_dir=Caminho.Caminho.IMG_TILE.value, tile_size=1024)
 
-    #segmentar_imagens()
+    segmentar_imagens()
 
-    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_TILE.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB", final_file_name="imagem_PNG_montada")
-    #remontar_rgb(tile_dir=Caminho.Caminho.IMG_MARK.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB_masked_output_0", final_file_name="mask_image_PNG")
+    remontar_rgb(tile_dir=Caminho.Caminho.IMG_TILE.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB", final_file_name="imagem_PNG_montada")
+    remontar_rgb(tile_dir=Caminho.Caminho.IMG_MARK.value, tile_height=1024, tile_width=1024, tiles_per_col=tiles[0]+1, tiles_per_row=tiles[1]+1, filler_color=(0, 0, 0), tile_name="RGB_masked_output_0", final_file_name="mask_image_PNG")
 
-    #apply_mask_in_chunks(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_nuvem_montada.png")
-    #apply_inverse_mask_in_chunks(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_sem_nuvem.png")
+    apply_mask_in_chunks(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_nuvem_montada.png")
+    apply_inverse_mask_in_chunks(image_path="./imagem_PNG_montada.png", mask_path="./mask_image_PNG.png", output_path="./imagem_final_cortada_sem_nuvem.png")
 
-    #remontar(img_png="./imagem_final_cortada_nuvem_montada.png", tiff_path=image, final_file_name="imagem_final_cortada_nuvem_montada")
-    #remontar(img_png="./imagem_final_cortada_sem_nuvem.png", tiff_path=image, final_file_name="imagem_final_cortada_sem_nuvem")
+    remontar(img_png="./imagem_final_cortada_nuvem_montada.png", tiff_path=image, final_file_name="imagem_final_cortada_nuvem_montada")
+    remontar(img_png="./imagem_final_cortada_sem_nuvem.png", tiff_path=image, final_file_name="imagem_final_cortada_sem_nuvem")
     
 
     #percent = porcentagem_nuvem(mask="./mask_image_PNG.png", img="./imagem_PNG_montada.png")
